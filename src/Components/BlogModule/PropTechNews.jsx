d2943a5cb3ac05df1570c16c6c8c649e030eafb2
ravindrauiet <ravindranathjha76@gmail.com>
2025-03-03 11:39:00 +0530

    resolve cicd problem

--- src/Components/BlogModule/PropTechNews.jsx
+++ src/Components/BlogModule/PropTechNews.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./PropTech.css";
+import "./Proptech.css";
 import { useNavigate } from "react-router-dom";
 
 const PropTechNews = () => {
