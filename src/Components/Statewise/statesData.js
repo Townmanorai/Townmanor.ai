c8480ca5f191649c4af6c86f06f8597bcb84ebba
Ayush3323~ <ayusht8799@gmail.com~>
2025-03-20 17:57:15 +0530

    state data

--- src/Components/Statewise/statesData.js
+++ src/Components/Statewise/statesData.js
@@ -5,175 +5,175 @@ export const statesData = [
     name: 'Andhra Pradesh',
     capital: 'Amaravati',
     population: '53.9M',
-    image: 'https://source.unsplash.com/800x600/?andhra-pradesh',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Tirumala_090615.jpg/250px-Tirumala_090615.jpg',
     link: '/state/andhra-pradesh'
   },
   {
     name: 'Arunachal Pradesh',
     capital: 'Itanagar',
     population: '1.5M',
-    image: 'https://source.unsplash.com/800x600/?arunachal-pradesh',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Golden_Pagoda_Namsai_Arunachal_Pradesh.jpg/330px-Golden_Pagoda_Namsai_Arunachal_Pradesh.jpg',
     link: '/state/arunachal-pradesh'
   },
   {
     name: 'Assam',
     capital: 'Dispur',
     population: '35.6M',
-    image: 'https://source.unsplash.com/800x600/?assam',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Kamakhya_Temple_-_DEV_8829.jpg/288px-Kamakhya_Temple_-_DEV_8829.jpg',
     link: '/state/assam'
   },
   {
     name: 'Bihar',
     capital: 'Patna',
     population: '124.8M',
-    image: 'https://source.unsplash.com/800x600/?bihar',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Temple_No.-_3%2C_Nalanda_Archaeological_Site.jpg/288px-Temple_No.-_3%2C_Nalanda_Archaeological_Site.jpg',
     link: '/state/bihar'
   },
   {
     name: 'Chhattisgarh',
     capital: 'Raipur',
     population: '29.4M',
-    image: 'https://source.unsplash.com/800x600/?chhattisgarh',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Bhoramdeo_Temple%2C_Kawardha.jpg/330px-Bhoramdeo_Temple%2C_Kawardha.jpg',
     link: '/state/chhattisgarh'
   },
   {
     name: 'Goa',
     capital: 'Panaji',
     population: '1.5M',
-    image: 'https://source.unsplash.com/800x600/?goa',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/BeachFun.jpg/288px-BeachFun.jpg',
     link: '/state/goa'
   },
   {
     name: 'Gujarat',
     capital: 'Gandhinagar',
     population: '70.4M',
-    image: 'https://source.unsplash.com/800x600/?gujarat',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Rani_ki_vav_02.jpg/268px-Rani_ki_vav_02.jpg',
     link: '/state/gujarat'
   },
   {
     name: 'Haryana',
     capital: 'Chandigarh',
     population: '28.1M',
-    image: 'https://source.unsplash.com/800x600/?haryana',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Cyber_City_View.jpg/250px-Cyber_City_View.jpg',
     link: '/state/haryana'
   },
   {
     name: 'Himachal Pradesh',
     capital: 'Shimla',
     population: '7.4M',
-    image: 'https://source.unsplash.com/800x600/?himachal-pradesh',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Kinnaur_Kailash.jpg/142px-Kinnaur_Kailash.jpg',
     link: '/state/himachal-pradesh'
   },
   {
     name: 'Jharkhand',
     capital: 'Ranchi',
     population: '38.6M',
-    image: 'https://source.unsplash.com/800x600/?jharkhand',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Shikharji_Parasnath_Giridih.jpg/288px-Shikharji_Parasnath_Giridih.jpg',
     link: '/state/jharkhand'
   },
   {
     name: 'Karnataka',
     capital: 'Bengaluru',
     population: '67.5M',
-    image: 'https://source.unsplash.com/800x600/?karnataka',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/UB_CITY_Skyline.jpg/275px-UB_CITY_Skyline.jpg',
     link: '/state/karnataka'
   },
   {
     name: 'Kerala',
     capital: 'Thiruvananthapuram',
     population: '35.7M',
-    image: 'https://source.unsplash.com/800x600/?kerala',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Athirapally_Waterfalls_after_the_Monsoons.jpg/268px-Athirapally_Waterfalls_after_the_Monsoons.jpg',
     link: '/state/kerala'
   },
   {
     name: 'Madhya Pradesh',
     capital: 'Bhopal',
     population: '85.3M',
-    image: 'https://source.unsplash.com/800x600/?madhya-pradesh',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Shri_Mahakaleshwar_Temple_Ujjain_-_panoramio_%284%29.jpg/288px-Shri_Mahakaleshwar_Temple_Ujjain_-_panoramio_%284%29.jpg',
     link: '/state/madhya-pradesh'
   },
   {
     name: 'Maharashtra',
     capital: 'Mumbai',
     population: '123.7M',
-    image: 'https://source.unsplash.com/800x600/?maharashtra',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Cave_26%2C_Ajanta.jpg/330px-Cave_26%2C_Ajanta.jpg',
     link: '/state/maharashtra'
   },
   {
     name: 'Manipur',
     capital: 'Imphal',
     population: '3.1M',
-    image: 'https://source.unsplash.com/800x600/?manipur',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Imphal%2C_Manipur_%2862%29.jpeg/182px-Imphal%2C_Manipur_%2862%29.jpeg',
     link: '/state/manipur'
   },
   {
     name: 'Meghalaya',
     capital: 'Shillong',
     population: '3.2M',
-    image: 'https://source.unsplash.com/800x600/?meghalaya',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Noakalikai_falls_1480244029215.jpg/280px-Noakalikai_falls_1480244029215.jpg',
     link: '/state/meghalaya'
   },
   {
     name: 'Mizoram',
     capital: 'Aizawl',
     population: '1.2M',
-    image: 'https://source.unsplash.com/800x600/?mizoram',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Embossed_Figures_Kawtchhuah_Ropui_Vangchhia_Mapuia_Hnamte_%281%29.JPG/288px-Embossed_Figures_Kawtchhuah_Ropui_Vangchhia_Mapuia_Hnamte_%281%29.JPG',
     link: '/state/mizoram'
   },
   {
     name: 'Nagaland',
     capital: 'Kohima',
     population: '2.2M',
-    image: 'https://source.unsplash.com/800x600/?nagaland',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Kohima_24_July_2021.jpeg/280px-Kohima_24_July_2021.jpeg',
     link: '/state/nagaland'
   },
   {
     name: 'Odisha',
     capital: 'Bhubaneswar',
     population: '46.3M',
-    image: 'https://source.unsplash.com/800x600/?odisha',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Konarka_Temple.jpg/1280px-Konarka_Temple.jpg',
     link: '/state/odisha'
   },
   {
     name: 'Punjab',
     capital: 'Chandigarh',
     population: '30.1M',
-    image: 'https://source.unsplash.com/800x600/?punjab',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Golden_Temple%2C_Amritsar%2C_Punjab_UNAG.jpg/260px-Golden_Temple%2C_Amritsar%2C_Punjab_UNAG.jpg',
     link: '/state/punjab'
   },
   {
     name: 'Rajasthan',
     capital: 'Jaipur',
     population: '79.3M',
-    image: 'https://source.unsplash.com/800x600/?rajasthan',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Thar_Khuri.jpg/330px-Thar_Khuri.jpg',
     link: '/state/rajasthan'
   },
   {
     name: 'Sikkim',
     capital: 'Gangtok',
     population: '0.7M',
-    image: 'https://source.unsplash.com/800x600/?sikkim',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Buddha_statue_at_Buddha_Park_of_Ravangla%2C_Sikkim%2C_India_%281%29.jpg/149px-Buddha_statue_at_Buddha_Park_of_Ravangla%2C_Sikkim%2C_India_%281%29.jpg',
     link: '/state/sikkim'
   },
   {
     name: 'Tamil Nadu',
     capital: 'Chennai',
     population: '77.8M',
-    image: 'https://source.unsplash.com/800x600/?tamil-nadu',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkCAlnaPX_YzTWEBrrUjXay3WlwOwYVBUHjw30xbHBU6vAxy50AAuX40Q&usqp=CAE&s',
     link: '/state/tamil-nadu'
   },
   {
     name: 'Telangana',
     capital: 'Hyderabad',
     population: '39.2M',
-    image: 'https://source.unsplash.com/800x600/?telangana',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Master_piece_of_ramappa_temple.jpg/250px-Master_piece_of_ramappa_temple.jpg',
     link: '/state/telangana'
   },
   {
     name: 'Tripura',
     capital: 'Agartala',
     population: '4.1M',
-    image: 'https://source.unsplash.com/800x600/?tripura',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk5ahPAOF8gMM_VdHaZII10zkjDPF0WfZly0j1ZSiniTKgzILGqp193mA&usqp=CAE&s',
     link: '/state/tripura'
   },
   {
@@ -187,14 +187,14 @@ export const statesData = [
     name: 'Uttarakhand',
     capital: 'Dehradun',
     population: '11.2M',
-    image: 'https://source.unsplash.com/800x600/?uttarakhand',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Auli%2C_India.jpg/288px-Auli%2C_India.jpg',
     link: '/state/uttarakhand'
   },
   {
     name: 'West Bengal',
     capital: 'Kolkata',
     population: '99.1M',
-    image: 'https://source.unsplash.com/800x600/?west-bengal',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Howrah_bridge_betwixt_Lights.jpg/275px-Howrah_bridge_betwixt_Lights.jpg',
     link: '/state/west-bengal'
   }
 ];
@@ -277,39 +277,182 @@ export const districtsData = {
   ],
   'uttar-pradesh': [
     {
-      name: 'Lucknow',
+      "name": 'Lucknow',
       population: '3.4M',
-      image: 'https://s7ap1.scene7.com/is/image/incredibleindia/1-chota-imambara-lucknow-uttar-pradesh-attr-hero?qlt=82&ts=1726648528039',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Gomti_Nagar.jpg/330px-Gomti_Nagar.jpg',
       link: 'https://lmc.up.nic.in/internet/user_loginnew.aspx',
       famousPlaces: ['Bara Imambara', 'Rumi Darwaza', 'Chota Imambara']
     },
     {
-      name: 'Kanpur',
-      population: '3.1M',
-      image: 'https://source.unsplash.com/800x600/?kanpur',
-      link: 'https://kmc.gov.in',
-      famousPlaces: ['JK Temple', 'Allen Forest Zoo', 'Kanpur Memorial Church']
+      name: "Kanpur",
+      population: "2.9M",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/J.K._Temple.jpg/330px-J.K._Temple.jpg",
+      link: "https://knnpropertytax.com/index.php",
+      famousPlaces: ["Allen Forest Zoo", "JK Temple", "Moti Jheel"]
+    },
+    {
+      name: "Varanasi",
+      population: "1.5M",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Varanasi%2C_India%2C_Ghats%2C_Cremation_ceremony_in_progress.jpg/250px-Varanasi%2C_India%2C_Ghats%2C_Cremation_ceremony_in_progress.jpg",
+      link: "http://nnvns.org.in/PropertyPayment/Mutation_Citizen/ViewPropInvoice.aspx",
+      famousPlaces: ["Kashi Vishwanath Temple", "Dashashwamedh Ghat", "Sarnath"]
+    },
+    {
+      name: "Agra",
+      population: "1.6M",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Taj_Mahal%2C_Agra%2C_India.jpg/263px-Taj_Mahal%2C_Agra%2C_India.jpg",
+      link: "https://agrapropertytax.com/",
+      famousPlaces: ["Taj Mahal", "Agra Fort", "Fatehpur Sikri"]
+    },
+    {
+      name: "Prayagraj",
+      population: "1.8M",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Allahabad_Montage_II_Dec_2014.jpg/800px-Allahabad_Montage_II_Dec_2014.jpg",
+      link: "https://allahabadmc.gov.in/CitizenHome.html#",
+      famousPlaces: ["Triveni Sangam", "Allahabad Fort", "Anand Bhavan"]
+    },
+    {
+      name: "Noida",
+      population: "0.9M",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Efficiency_in_the_shape.jpg/96px-Efficiency_in_the_shape.jpg",
+      link: "https://services.india.gov.in/service/detail/pay-house-tax-uttar-pradesh",
+      famousPlaces: ["DLF Mall of India", "Okhla Bird Sanctuary", "Worlds of Wonder"]
+    },
+    {
+      name: "Ghaziabad",
+      population: "1.7M",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Indirapuram.jpg/238px-Indirapuram.jpg",
+      link: "https://ghaziabad.nic.in/en/service/property-tax/",
+      famousPlaces: ["Swarn Jayanti Park", "City Forest", "ISKCON Ghaziabad"]
+    },
+    {
+      name: "Meerut",
+      population: "1.5M",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Meerut_Montage_01.jpg/250px-Meerut_Montage_01.jpg",
+      link: "https://ptaxmnn.com/",
+      famousPlaces: ["Augarnath Temple", "Suraj Kund", "Gandhi Bagh"]
+    },
+    {
+      name: "Aligarh",
+      population: "1.2M",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Aligarh_Habitat_Center.jpg/137px-Aligarh_Habitat_Center.jpg",
+      link: "https://services.india.gov.in/service/detail/property-tax-for-aligarh-municipal-corporation-uttar-pradesh-1",
+      famousPlaces: ["Aligarh Fort", "Khereshwar Temple", "Maulana Azad Library"]
+    },
+    {
+      name: "Gorakhpur",
+      population: "0.7M",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Ramgarh_Tal_Lake%2C_Gorakhpur%2C_8_Apr_2019.jpg/220px-Ramgarh_Tal_Lake%2C_Gorakhpur%2C_8_Apr_2019.jpg",
+      link: "https://gorakhpurnagarnigam.up.gov.in/PaymentBill.aspx",
+      famousPlaces: ["Gorakhnath Temple", "Gita Press", "Ramgarh Tal"]
+    },
+    {
+      name: "Mathura",
+      population: "0.6M",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Vishram_Ghat.jpg/268px-Vishram_Ghat.jpg",
+      link: "https://mathura.upptax.org/public/paytax",
+      famousPlaces: ["Shri Krishna Janmabhoomi", "Dwarkadhish Temple", "Govardhan Hill"]
+    },
+    {
+      name: "Bareilly",
+      population: "0.9M",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Chunna_Miyan_Temple_Bareilly_02.jpg/1024px-Chunna_Miyan_Temple_Bareilly_02.jpg",
+      link: "https://services.india.gov.in/service/detail/property-tax-for-bareilly-municipal-corporation-uttar-pradesh-1",
+      famousPlaces: ["Alakhnath Temple", "Rambagh Garden", "Bareilly Fort"]
+    },
+    {
+      name: "Sultanpur",
+      population: "0.5M",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo5n8ylfn0YXFxCXNbwW55ZdwheaX84RYaFHfHExSIo6VvM0iW0GZvhnU&usqp=CAE&s",
+      link: "https://nppsultanpur.in/payment",
+      famousPlaces: ["Sultanpur Bird Sanctuary", "Shivgadi Mandir", "Chandpur"]
+    },
+    {
+      name: "Moradabad",
+      population: "0.8M",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR41u2bYUsW8mlaC-D4j5D8T2NzhrLgmdmQbZElpR4RbB-jtMeX-PSk94Q&usqp=CAE&s",
+      link: "https://tax.onlinemnn.com/SearchHouseDetailsF.aspx",
+      famousPlaces: ["Mandir Pahadi", "Siddheshwar Temple", "Bheemgoda Barrage"]
+    },
+    {
+      name: "Jhansi",
+      population: "0.5M",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAQb2a340S4FmpFQ4inlqa_CjPjQVBotCs5q3niiQ3ysC2cmnAVXqrc9I&usqp=CAE&s",
+      link: "https://www.jhansipropertytax.com/pay_tax.php",
+      famousPlaces: ["Jhansi Fort", "Rani Mahal", "Barua Sagar"]
+    }
+  
+
+  ],
+  'delhi': [
+    {
+      "name": "Central Delhi",
+      "population": "0.67M",
+      "image": "https://lh5.googleusercontent.com/proxy/GuuP9z9f8AKP6XBhTDH-3553S3iYXWOmXW0mj0fOzIGwzOj6agHwkQykP2dlphSEKYAEe-i_YVqVwBvSs_Figq_UdbfHxbwlzHmdnqYX03fVg1SZNTtiWb-IOE_kKN-d2wMXX3KztkwY5xB0uz61-adaZpsSwSw=w540-h312-n-k-no",
+      "link": "https://mcdonline.nic.in/portal",
+      "famousPlaces": ["Raisina Hill", "Jantar Mantar", "Connaught Place"]
+    },
+    {
+      "name": "East Delhi",
+      "population": "1.97M",
+      "image": "https://cdn.s3waas.gov.in/s3f79921bbae40a577928b76d2fc3edc2a/uploads/2018/05/2020052139.jpg",
+      "link": "https://mcdonline.nic.in/ptrmcd/web/citizen/info",
+      "famousPlaces": ["Akshardham Temple", "ISKCON Temple", "Laxmi Nagar"]
+    },
+    {
+      "name": "New Delhi",
+      "population": "0.16M",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLU-lfZA2VTcRngyDEHXfuhrzs9-SLB9qoZHIDmrI4dk1FhvCR1EipeSE&usqp=CAE&s",
+      "link": "https://online.ndmc.gov.in/ptis_dues/",
+      "famousPlaces": ["India Gate", "Rashtrapati Bhavan", "Connaught Place"]
+    },
+    {
+      "name": "North Delhi",
+      "population": "1.02M",
+      "image": "https://lh5.googleusercontent.com/p/AF1QipMT_c2770FFh7pQLEuJsdM0aWlR9el2Zn7gnc8G=w540-h312-n-k-no",
+      "link": "https://online.ndmc.gov.in/ptis_dues/",
+      "famousPlaces": ["Red Fort", "Chandni Chowk", "Jama Masjid"]
+    },
+    {
+      "name": "North East Delhi",
+      "population": "2.58M",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Delhi_districts.png/300px-Delhi_districts.png",
+      "link": "https://northeastdelhi.gov.in",
+      "famousPlaces": ["Seelampur", "Gokulpuri", "Shahdara"]
+    },
+    {
+      "name": "North West Delhi",
+      "population": "4.20M",
+      "image": "",
+      "link": "https://mcdonline.nic.in/portal",
+      "famousPlaces": ["Dilli Haat", "Peera Garhi", "Punjabi Bagh"]
+    },
+    {
+      "name": "South Delhi",
+      "population": "3.14M",
+      "image": "https://lh5.googleusercontent.com/p/AF1QipP6nQRzxBQEbaa3SFF04aexX4PNu6imkm1HAwF6=w540-h312-n-k-no",
+      "link": "https://mcdonline.nic.in/portal",
+      "famousPlaces": ["Qutub Minar", "Lotus Temple", "Hauz Khas Village"]
+    },
+    {
+      "name": "South West Delhi",
+      "population": "2.64M",
+      "image": "https://lh5.googleusercontent.com/p/AF1QipMlYQw03lsIOVK8WcdF37_P5rEnotK6UKC9sLeo=w540-h312-n-k-no",
+      "link": "https://online.ndmc.gov.in/ptis_dues/",
+      "famousPlaces": ["Dwarka", "Najafgarh", "Lodi Garden"]
+    },
+    {
+      "name": "West Delhi",
+      "population": "2.92M",
+      "image": "https://dcwestrev.delhi.gov.in/sites/default/files/dcwestrev/intro/deputy_commissioner_west.jpg",
+      "link": "https://online.ndmc.gov.in/ptis_dues/",
+      "famousPlaces": ["Mughal Garden", "Raja Garden", "Tihar Jail"]
     }
   ]
 };
 
 export const utDistrictsData = {
-  'delhi': [
-    {
-      name: 'New Delhi',
-      population: '257K',
-      image: 'https://source.unsplash.com/800x600/?new-delhi',
-      link: 'https://ndmc.gov.in',
-      famousPlaces: ['India Gate', 'Rashtrapati Bhavan', 'Connaught Place']
-    },
-    {
-      name: 'North Delhi',
-      population: '887K',
-      image: 'https://source.unsplash.com/800x600/?north-delhi',
-      link: 'https://northdelhi.gov.in',
-      famousPlaces: ['Red Fort', 'Chandni Chowk', 'Jama Masjid']
-    }
-  ],
+  
   'chandigarh': [
     {
       name: 'Chandigarh',
